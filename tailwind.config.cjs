8378bb25b5575f629aab48455ee97ec1ffb27d85
NicoC0 <nicoconfaa@gmail.com>
2022-08-21 18:53:52 -0300

    initial setup

---
+++ tailwind.config.cjs
@@ -0,0 +1,31 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        primary: '#E5E5E5',
+        secondary: '#EDF7FA',
+        buttonColor: '#FF6464',
+        darkText: '#21243D',
+        darkBlue: '#142850',
+        lightBlue: '#00A8CC',
+        lightGray: '#8695A4',
+      },
+      fontFamily: {
+        heebo: ['Heebo', 'sans-serif']
+      },
+    },
+    screens: {
+      xs: "480px",
+      ss: "620px",
+      sm: "768px",
+      md: "1060px",
+      lg: "1200px",
+      xl: "1700px",
+    },
+  },
+  plugins: [],
+}
