f3c0af01c98484030cc52f8002b5adf447ef566a
NicoC0 <nicoconfaa@gmail.com>
2022-08-21 21:39:43 -0300

    refactor: navLinks in constants

---
+++ src/constants/index.js
@@ -0,0 +1,18 @@
+export const navLinks = [
+  {
+    id: "home",
+    title: "Home"
+  },
+  {
+    id: 'works',
+    title: 'Works'
+  },
+  {
+    id: "blog",
+    title: "Blog"
+  },
+  {
+    id: "contact",
+    title: "Contact"
+  }
+]
