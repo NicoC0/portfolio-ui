935249d4ad692927a872cd35875a2282c9c1d090
NicoC0 <nicoconfaa@gmail.com>
2022-08-21 22:58:07 -0300

    refactor: rendering hero

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,17 @@
 import React from 'react'
 import styles from './style'
 import Navbar from './components/Navbar'
+import Hero from './components/Hero'
 
 function App() {
   return (
     <div className='bg-primary w-full overflow-hidden'>
         <div className='flex flex-row'>
           <Navbar />
+        </div>
+
+        <div className={`${styles.flexCenter} py-40 flex-row`}>
+          <Hero />
         </div>
     </div>
   )
